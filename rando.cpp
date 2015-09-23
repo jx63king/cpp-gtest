dd1879be8df83840cee4cd6036182dcf68342a38
Shirley <jx63king@live.cn>
2015-09-23 03:06:34 -0700

    Updated rando.cpp with proper functions. Created TEST cases for three functions in RandoTest

--- rando.cpp
+++ rando.cpp
@@ -1,42 +1,72 @@
 #include "rando.h"
+#include <iostream>
 //#include <stdlib.h>
 
+using namespace std;
 
 /**
  * Worry if only one child is smiling or if all children are smiling
-**/
+ **/
 bool Rando::shouldWorry(bool childASmile, bool childBSmile, bool childCSmile)
 {
-	if( childASmile && childBSmile && childCSmile )
-		return true;
-	else if( childASmile ^ childBSmile ^ childCSmile )
-		return true;
-	else
-		return false;
+  if( childASmile && childBSmile && childCSmile )
+    return true;
+  else if( childASmile ^ childBSmile ^ childCSmile )
+    return true;
+  else
+    return false;
 }
 /**
  * Determines if first or second are evenly divisible by the other.
-**/
+ **/
 bool Rando::isDivisbleBy(int first, int second)
 {
-	return false;
+  int result = first % second;
+
+  if(result == 0)
+    return true;
+  else
+    return false;
 }
 
 /**
  * Given a number, num, says whether or not that number is prime
  * (that is, only evenly divisible by itself and 1. For the sake
  * of this function, do NOT consider 0 prime)
-**/
+ **/
 bool Rando::isPrime(int num)
 {
-	return true;
+  int i = 1;
+  int j=0;
+  while(num != i)
+  {
+    if(num % i ==0)
+      j=j+1;
+
+    i++;
+  }
+  if(j<2)
+  {
+    //cout << num  << " is a prime number" << endl;
+    return true;
+  }
+  else
+  { 
+    //cout << num  << " is not a prime number" << endl;
+    return false;
+  }
 }
 
 /**
  * Given two numbers, first and second, return the value of the one
  * that is closest to zero without actually being zero.
-**/
+ **/
 int Rando::nearestToZero(int a, int b)
-{
-	return 0;
+{ 
+  if((abs(a) - 0) > (abs(b)))
+    return b;
+  if((abs(a) - 0) < (abs(b)))
+    return a;
+
+  //return 0;
 }
--- randoTest.cpp
+++ randoTest.cpp
@@ -1,30 +1,115 @@
 /**
  * Unit Tests for Rando Test
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "rando.h"
 
 /*  
-	FUNCTIONS WE NEED TO TEST:
+    FUNCTIONS WE NEED TO TEST:
 
-	bool shouldWorry(bool,bool,bool);
-	bool isDivisbleBy(int,int);
-	bool isPrime(int);
-	int nearestToZero(int,int);
-*/
- 
+    bool shouldWorry(bool,bool,bool);
+    bool isDivisbleBy(int,int);
+    bool isPrime(int);
+    int nearestToZero(int,int);
+    */
+
 class RandoTest : public ::testing::Test
 {
-	protected:
-		RandoTest(){} //constructor runs before each test
-		virtual ~RandoTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+  protected:
+    RandoTest(){} //constructor runs before each test
+    virtual ~RandoTest(){} //destructor cleans up after tests
+    virtual void SetUp(){} //sets up before each test (after constructor)
+    virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
 TEST(RandoTest, allChildrenSmile)
 {
-	Rando rando;
-	ASSERT_TRUE( rando.shouldWorry(true,true,true) );
+  Rando rando;
+  ASSERT_TRUE( rando.shouldWorry(true,true,true) );
+}
+
+
+//TEST Case 1 isDivisbleBy
+TEST(RandoTest, Division_One)
+{
+  Rando rando;
+  ASSERT_TRUE(rando.isDivisbleBy(20,10));
+}
+
+//TEST Case 2 isDivisbleBy
+TEST(RandoTest, Division_Two)
+{
+  Rando rando;
+  ASSERT_FALSE(rando.isDivisbleBy(20,3));
+}
+
+//TEST Case 1 isPrime
+TEST(RandoTest, Prime_One)
+{
+  Rando rando;
+  ASSERT_TRUE(rando.isPrime(2));
+}
+
+
+
+//TEST Case 2 isPrime
+TEST(RandoTest, Prime_Two)
+{
+  Rando rando;
+  ASSERT_FALSE(rando.isPrime(4));
+}
+
+//TEST Case 3 isPrime
+TEST(RandoTest, Prime_Three)
+{
+  Rando rando;
+  ASSERT_TRUE(rando.isPrime(5));
+}
+
+//TEST Case 4 isPrime
+TEST(RandoTest, Prime_Four)
+{
+  Rando rando;
+  ASSERT_FALSE(rando.isPrime(121));
+}
+
+//TEST Case 5 isPrime
+TEST(RandoTest, Prime_Five)
+{
+  Rando rando;
+  ASSERT_FALSE(rando.isPrime(99999999));
+}
+
+//TEST Case 1 nearestToZero
+TEST(RandoTest, nearestToZero_One)
+{
+  Rando rando;
+  int result = rando.nearestToZero(3,2);
+  ASSERT_TRUE(result == 2);
+}
+
+//TEST Case 2 nearestToZero
+TEST(RandoTest, nearestToZero_Two)
+{
+  Rando rando;
+  int result = rando.nearestToZero(-200,-22);
+  ASSERT_TRUE(result == -22);
+}
+
+//TEST Case 3 nearestToZero
+TEST(RandoTest, nearestToZero_Three)
+{
+  Rando rando;
+  int result = rando.nearestToZero(-200,22);
+  ASSERT_TRUE(result == 22);
+}
+
+
+//TEST Case 4 nearestToZero
+TEST(RandoTest, nearestToZero_Four)
+{
+  Rando rando;
+  int result = rando.nearestToZero(200,-22);
+  ASSERT_TRUE(result == -22);
 }
